e94491ab17b543320cd4bb5064eef63fbc79a088
Wendel Albino <wendelalbino@compliancefiscal.com.br>
2021-02-08 10:03:21 -0300

    Redmine #75771
    -- Em 04/02/2021   - Wendel Albino - patch 2.9.5.5 / 2.9.6-2 e release 297
    -- Redmine #75771  - Verificar agendamento de integracao SPANI
    -- Rotina Alterada - pkb_integr_cad_geral / pkb_integr_empresa_geral ->inclusao nos sleects de multorg com rownum (nro_linhas)
    --                 -  para nao executar mais de uma vez as procedures por multiorg.

--- Patch_2.9.6.2/Packages/catalogar.sql
+++ Patch_2.9.6.2/Packages/catalogar.sql
@@ -4,6 +4,13 @@ prompt Catalogar Packages
 
 prompt -----------------------------------------------------------------------------------
 
+prompt 62a_pk_int_view_cad
+@@62a_pk_int_view_cad.pks
+show err
+prompt 62b_pk_int_view_cad
+@@62b_pk_int_view_cad.pkb
+show err
+
 prompt 251a_pk_gera_arq_gia
 @@251a_pk_gera_arq_gia.pks
 show err
@@ -11,6 +18,5 @@ prompt 251b_pk_gera_arq_gia
 @@251b_pk_gera_arq_gia.pkb
 show err
 
-show err
 Prompt Catalogar Objetos Invalidos
 @@rec_obj_invalid.sql
