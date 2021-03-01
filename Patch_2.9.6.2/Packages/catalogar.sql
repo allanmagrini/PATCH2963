e94a6b61902fdb9b8472bbcf852df957bed2580e
Marcos_Ferreira <marcos.ferreira@compliancefiscal.com.br>
2021-03-01 15:41:35 -0300

    #70422
    #70422

--- Patch_2.9.6.2/Packages/catalogar.sql
+++ Patch_2.9.6.2/Packages/catalogar.sql
@@ -4,31 +4,59 @@ prompt Catalogar Packages
 
 prompt -----------------------------------------------------------------------------------
 
+prompt 30a_CSF_OWN_pk_csf
+@@30a_CSF_OWN_pk_csf.pks
+show err
+prompt 30b_CSF_OWN_pk_csf
+@@30b_CSF_OWN_pk_csf.pkb
+show err
+
 prompt 31a_CSF_OWN_pk_csf_api
 @@31a_CSF_OWN_pk_csf_api.pks
 show err
 prompt 31b_CSF_OWN_pk_csf_api
 @@31b_CSF_OWN_pk_csf_api.pkb
 show err
-
+ 
 prompt 33a_CSF_OWN_pk_valida_ambiente
 @@33a_CSF_OWN_pk_valida_ambiente.pks
 show err
-prompt 33b_CSF_OWN_pk_valida_ambiente
+prompt 33b_CSF_OWN_pk_valida_ambiente 
 @@33b_CSF_OWN_pk_valida_ambiente.pkb
 show err
 
-prompt 40a_CT_CSF_OWN_pk_csf_api_ct
+prompt 34a_csf_own_pk_integr_view
+@@34a_csf_own_pk_integr_view.pks
+show err
+prompt 34b_csf_own_pk_integr_view
+@@34b_csf_own_pk_integr_view.pkb
+show err
+
+prompt 39a_pk_csf_ct.pks
+@@39a_pk_csf_ct.pks
+show err
+prompt 39b_pk_csf_ct.pkb
+@@39b_pk_csf_ct.pkb
+show err
+
+prompt 40a_CT_CSF_OWN_pk_csf_api_ct.pks
 @@40a_CT_CSF_OWN_pk_csf_api_ct.pks
 show err
-prompt 40b_CT_CSF_OWN_pk_csf_api_ct
+prompt 40b_CT_CSF_OWN_pk_csf_api_ct.pkb 
 @@40b_CT_CSF_OWN_pk_csf_api_ct.pkb
 show err
 
-prompt 42a_csf_own_pk_integr_view_ct
+prompt 41a_CSF_OWN_pk_valida_ambiente_ct.pks
+@@41a_CSF_OWN_pk_valida_ambiente_ct.pks
+show err
+prompt 41b_CSF_OWN_pk_valida_ambiente_ct.pkb
+@@41b_CSF_OWN_pk_valida_ambiente_ct.pkb
+show err
+
+prompt 42a_csf_own_pk_integr_view_ct.pks
 @@42a_csf_own_pk_integr_view_ct.pks
 show err
-prompt 42b_csf_own_pk_integr_view_ct
+prompt 42b_csf_own_pk_integr_view_ct.pkb
 @@42b_csf_own_pk_integr_view_ct.pkb
 show err
 
@@ -46,17 +74,81 @@ prompt 62b_pk_int_view_cad
 @@62b_pk_int_view_cad.pkb
 show err
 
+prompt 65a_pk_csf_api_d100.pks
+@@65a_pk_csf_api_d100.pks
+show err
+prompt 65b_pk_csf_api_d100.pkb 
+@@65b_pk_csf_api_d100.pkb
+show err
+
 prompt 73a_pk_gera_arq_efd
 @@73a_pk_gera_arq_efd.pks
 show err
 prompt 73b_pk_gera_arq_efd
 @@73b_pk_gera_arq_efd.pkb
+show err
+
+prompt 74a_pk_apur_icms
+@@74a_pk_apur_icms.pks
+show err
+prompt 74b_pk_apur_icms
+@@74b_pk_apur_icms.pkb
+show err
+
+prompt 76a_pk_apur_icms_st
+@@76a_pk_apur_icms_st.pks
+show err
+prompt 76b_pk_apur_icms_st
+@@76b_pk_apur_icms_st.pkb
+show err
+
+prompt 120a_pk_csf_efd_pc.pks
+@@120a_pk_csf_efd_pc.pks
+show err
+prompt 120b_pk_csf_efd_pc.pkb
+@@120b_pk_csf_efd_pc.pkb
+show err
+
+prompt 121a_pk_gera_arq_efd_pc
+@@121a_pk_gera_arq_efd_pc.pks
+show err
+prompt 121b_pk_gera_arq_efd_pc
+@@121b_pk_gera_arq_efd_pc.pkb
+show err
+
+prompt 140a_pk_apur_iss
+@@140a_pk_apur_iss.pks
+show err
+prompt 140b_pk_apur_iss
+@@140b_pk_apur_iss.pkb
+show err
 
 prompt 148a_pk_csf_api_ddo.pks
 @@148a_pk_csf_api_ddo.pks
 show err
 prompt 148b_pk_csf_api_ddo.pkb
 @@148b_pk_csf_api_ddo.pkb
+show err
+
+prompt 190a_pk_rel_erro_agend_integr
+@@190a_pk_rel_erro_agend_integr.pks
+show err
+prompt 190b_pk_rel_erro_agend_integr
+@@190b_pk_rel_erro_agend_integr.pkb
+show err
+
+prompt 211a_pk_despr_integr.pks
+@@211a_pk_despr_integr.pks
+show err
+prompt 211b_pk_despr_integr.pkb
+@@211b_pk_despr_integr.pkb
+show err
+
+prompt 221a_csf_own_pk_csf_api_nfs.pks
+@@221a_csf_own_pk_csf_api_nfs.pks
+show err
+prompt 221b_csf_own_pk_csf_api_nfs.pkb
+@@221b_csf_own_pk_csf_api_nfs.pkb
 show err
 
 prompt 251a_pk_gera_arq_gia
@@ -66,11 +158,46 @@ prompt 251b_pk_gera_arq_gia
 @@251b_pk_gera_arq_gia.pkb
 show err
 
+prompt 320a_pk_gera_arq_sint
+@@320a_pk_gera_arq_sint.pks
+show err
+prompt 320b_pk_gera_arq_sint
+@@320b_pk_gera_arq_sint.pkb
+show err
+
+prompt 341a_pk_csf_api_dirf
+@@341a_pk_csf_api_dirf.pks
+show err
+prompt 341b_pk_csf_api_dirf
+@@341b_pk_csf_api_dirf.pkb
+show err
+
+prompt 346a_pk_gera_inf_rend_dirf
+@@346a_pk_gera_inf_rend_dirf.pks
+show err
+prompt 346b_pk_gera_inf_rend_dirf
+@@346b_pk_gera_inf_rend_dirf.pkb
+show err
+
+prompt 371a_pk_csf_api_pgto_imp_ret.pks
+@@371a_pk_csf_api_pgto_imp_ret.pks
+show err
+prompt 371b_pk_csf_api_pgto_imp_ret.pkb
+@@371b_pk_csf_api_pgto_imp_ret.pkb
+show err
+
 prompt 420a_pk_subapur_icms
 @@420a_pk_subapur_icms.pks
 show err
 prompt 420b_pk_subapur_icms
 @@420b_pk_subapur_icms.pkb
+show err
+
+prompt 421a_pk_apur_icms_difal
+@@421a_pk_apur_icms_difal.pks
+show err
+prompt 421b_pk_apur_icms_difal
+@@421b_pk_apur_icms_difal.pkb
 show err
 
 prompt 460a_pk_csf_secf
@@ -87,11 +214,81 @@ prompt 467b_pk_calc_apur_lr
 @@467b_pk_calc_apur_lr.pkb
 show err
 
+prompt 555a_pk_csf_gpi
+@@555a_pk_csf_gpi.pks
+show err
+prompt 555b_pk_csf_gpi
+@@555b_pk_csf_gpi.pkb
+show err
+
+prompt 556a_pk_csf_api_gpi
+@@556a_pk_csf_api_gpi.pks
+show err
+prompt 556b_pk_csf_api_gpi
+@@556b_pk_csf_api_gpi.pkb
+show err
+
+prompt 557a_pk_vld_amb_gpi
+@@557a_pk_vld_amb_gpi.pks
+show err
+prompt 557b_pk_vld_amb_gpi
+@@557b_pk_vld_amb_gpi.pkb
+show err
+
+prompt 560a_pk_ger_guia_pgto_imp
+@@560a_pk_ger_guia_pgto_imp.pks
+show err
+prompt 560b_pk_ger_guia_pgto_imp
+@@560b_pk_ger_guia_pgto_imp.pkb
+show err
+
 prompt 610a_pk_rel_apur_irpj_csll_parc
 @@610a_pk_rel_apur_irpj_csll_parc.pks
 show err
 prompt 610b_pk_rel_apur_irpj_csll_parc
 @@610b_pk_rel_apur_irpj_csll_parc.pkb
+show err
+
+prompt 620a_pk_apur_iss_blc_b
+@@620a_pk_apur_iss_blc_b.pks
+show err
+prompt 620b_pk_apur_iss_blc_b
+@@620b_pk_apur_iss_blc_b.pkb
+show err
+
+prompt 636a_pk_relac_ped_nf.pks
+@@636a_pk_relac_ped_nf.pks
+show err
+prompt 636b_pk_relac_ped_nf.pkb
+@@636b_pk_relac_ped_nf.pkb
+show err
+
+prompt 637a_pk_relac_ped_ct.pks
+@@637a_pk_relac_ped_ct.pks
+show err
+prompt 637b_pk_relac_ped_ct.pkb
+@@637b_pk_relac_ped_ct.pkb
+show err
+
+prompt 643a_pk_int_view_bloco_m_pc.pks
+@@643a_pk_int_view_bloco_m_pc.pks
+show err
+prompt 643b_pk_int_view_bloco_m_pc.pkb
+@@643b_pk_int_view_bloco_m_pc.pkb
+show err
+
+prompt 651a_CSF_OWN_pk_csf_api_nfce.pks
+@@651a_CSF_OWN_pk_csf_api_nfce.pks
+show err
+prompt 651b_CSF_OWN_pk_csf_api_nfce.pkb
+@@651b_CSF_OWN_pk_csf_api_nfce.pkb
+show err
+
+prompt 653a_csf_own_pk_integr_view_nfce
+@@653a_csf_own_pk_integr_view_nfce.pks
+show err
+prompt 653b_csf_own_pk_integr_view_nfce
+@@653b_csf_own_pk_integr_view_nfce.pkb
 show err
 
 prompt 666a_pk_gera_lucro_expl
@@ -105,6 +302,10 @@ prompt 667_PB_CORRIGIR_PESSOA_NF_CT
 @@667_PB_CORRIGIR_PESSOA_NF_CT.prc
 show err
 
+prompt 669_pb_job_trunca_relnf
+@@669_pb_job_trunca_relnf.prc
+show err
+
 prompt 668a_pk_rel_lucro_expl
 @@668a_pk_rel_lucro_expl.pks
 show err
@@ -112,5 +313,10 @@ prompt 668b_pk_rel_lucro_expl
 @@668b_pk_rel_lucro_expl.pkb
 show err
 
+prompt pb_gera_cenario.prc
+@@pb_gera_cenario.prc
+show err
+
+show err
 Prompt Catalogar Objetos Invalidos
 @@rec_obj_invalid.sql
